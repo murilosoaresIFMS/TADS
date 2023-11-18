0e037877641542d5178176ab02978662ea0616d9
murilosoaresIFMS <murilo.oliveira4@estudante.ifms.edu.br>
2023-11-17 20:56:14 -0400

    []

---
+++ TADS-SEM1/tads-algoritmos/aula14 - funcoes/c14e02.py
@@ -0,0 +1,9 @@
+# Receber dois números e devolver sua soma.
+
+def soma (a,b):
+    return a + b
+
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite um número: "))
+
+print(soma(n1,n2))
---
+++ TADS-SEM1/tads-algoritmos/aula14 - funcoes/exemplo_de_modulo/programa.py
@@ -0,0 +1,7 @@
+import modulo_simples 
+
+a = int(input("Digite um número: "))
+b = int(input("Digite um número: "))
+c = modulo_simples.soma(a,b)
+
+print(f'A soma de {a} com {b} é {c}')
