cee940e8259cebe5147c74ef5f4641b89d3ccbdd
murilosoaresIFMS <150303360+murilosoaresIFMS@users.noreply.github.com>
2024-07-25 22:14:03 -0400

    Add files via upload

---
+++ TADS-SEM2/web/aula 25jul/script.js
@@ -0,0 +1,74 @@
+/*
+// formas de declarar variavel
+const a = 1; // constante
+let b = 2;  // variável
+var c = 3; // legado
+d = 10;
+
+// condicional
+if (a > 2) {
+    // caso verdade
+} else {
+    // caso falso
+}
+
+// apresentacao dos resultados
+console.log(a)
+alert(a)
+document.write(a)
+
+
+// manipulacao de DOM
+
+let c = document.getElementById('container');
+let d = document.getElementsByClassName("menu");
+let e = document.getElementsByTagName("input");
+let f = document.querySelector('#container');
+
+// estrutura de repetiçao
+
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+let j = 0;
+while (j < 10) {
+    console.log(j)
+    j++;
+}
+
+let h = 0;
+do {
+    console.log(h);
+    h++;
+} while (h < 10);
+
+function soma(a,b) {
+    console.log(a+b)
+}
+
+soma(6,6);
+
+let soma = new Function("b", "h", "return (a + b")
+
+// objetos
+*/
+
+let pessoa = {
+    nome: "João",
+    idade: 20, 
+    sexo: "M"
+}
+
+function dizerOi() {
+    let nome = document.getElementById("nome").value;
+    let error = document.getElementById('Error');
+    console.log(error)
+    if (nome.length != 0) {
+        console.log(`Oi ${nome}`)
+    }
+    else {
+        error.innerHTML = 'Insira um valor válido'
+        error.classList.add("Error")
+    }
+}
